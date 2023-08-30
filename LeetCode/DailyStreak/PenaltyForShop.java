f395cf491a84eeb330ee7ce427e753fb052e5408
AanshOjha <aanshojha185@gmail.com>
2023-08-30 20:58:40 +0530

    Hard

--- LeetCode/DailyStreak/PenaltyForShop.java
+++ LeetCode/DailyStreak/PenaltyForShop.java
@@ -3,9 +3,9 @@ package LeetCode.DailyStreak;
 public class PenaltyForShop {
     static public int bestClosingTime(String customers) {
         int max_score = 0, score = 0, best_hour = -1;
-        for(int i = 0; i < customers.length(); ++i) {
+        for (int i = 0; i < customers.length(); ++i) {
             score += (customers.charAt(i) == 'Y') ? 1 : -1;
-            if(score > max_score) {
+            if (score > max_score) {
                 max_score = score;
                 best_hour = i;
             }
