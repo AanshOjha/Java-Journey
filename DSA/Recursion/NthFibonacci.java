52dc941eb1739383674532faf97091842aef25db
AanshOjha <aanshojha185@gmail.com>
2023-09-08 10:03:07 +0530

    Fibo formula

--- DSA/Recursion/NthFibonacci.java
+++ DSA/Recursion/NthFibonacci.java
@@ -2,7 +2,17 @@ package DSA.Recursion;
 
 public class NthFibonacci {
     public static void main(String[] args) {
-        System.out.println(fibonacci(7));   // 13
+        //System.out.println(fibonacci(7));   // 13
+        System.out.println(fiboFormula(3));
+    }
+
+
+    // As this is direct formula so can find 50th even 100th fibo number!
+    static int fiboFormula(int n) {
+        return (int) (
+                (Math.pow(((1 + Math.sqrt(5)) / 2), n) //
+                        - Math.pow(((1 - Math.sqrt(5)) / 2), n)
+                ) / Math.sqrt(5));
     }
 
     // This has time complexity of around 2 to power N
