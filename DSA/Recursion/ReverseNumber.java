62a33822613d76d9f06915dbaad72ecf43fe2d29
AanshOjha <aanshojha185@gmail.com>
2023-09-10 16:35:32 +0530

    Reverse

--- DSA/Recursion/ReverseNumber.java
+++ DSA/Recursion/ReverseNumber.java
@@ -2,7 +2,7 @@ package DSA.Recursion;
 
 public class ReverseNumber {
     static int reverse(int n) {
-        int digits = (int) (Math.log10(n)) + 1;
+        int digits = (int) (Math.log10(n)) + 1;     // Special formula
         return helper(n, digits);
     }
 
@@ -12,10 +12,10 @@ public class ReverseNumber {
         }
 
         int remainder = n % 10;
-        return remainder * (int) (Math.pow(10, digits - 1)) + helper(n / 10, digits - 1);
+        return (int) ((remainder * Math.pow(10, digits - 1)) + helper(n / 10, digits - 1));
     }
 
     public static void main(String[] args) {
-        System.out.println(reverse(1234));
+        System.out.println(reverse(5678));
     }
 }
