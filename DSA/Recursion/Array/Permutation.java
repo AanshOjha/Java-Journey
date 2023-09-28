935a35df21bf1fcfa792ea50a03eafb2a922f106
AanshOjha <aanshojha185@gmail.com>
2023-09-28 15:49:36 +0530

    Permutations

--- DSA/Recursion/Array/Permutation.java
+++ DSA/Recursion/Array/Permutation.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Permutation {
     public static void main(String[] args) {
-//        System.out.println(permutationRet("", "ab"));
-        permutation("", "abc");
+        System.out.println(permutationCount2("", "abc"));
+//        permutation("", "abc");
     }
 
     static void permutation(String p, String up) {
@@ -39,5 +39,44 @@ public class Permutation {
         }
 
         return ans;     // Returns empty element
+    }
+
+
+    static int count = 0;
+    static int permutationCount(String p, String up) {
+
+        if (up.isEmpty()) {
+            count++;
+            return count;
+        }
+
+        char ch = up.charAt(0);
+
+        for (int i = 0; i <= p.length(); i++) {     // i max upto p.length()
+            String f = p.substring(0, i);
+            String s = p.substring(i);  // i -> end
+            permutationCount(f + ch + s, up.substring(1));
+        }
+
+        return count;
+    }
+
+//   ========================= OR =========================
+
+    static int permutationCount2(String p, String up) {
+        int count2 = 0;
+        if (up.isEmpty()) {
+            return 1;
+        }
+
+        char ch = up.charAt(0);
+
+        for (int i = 0; i <= p.length(); i++) {     // i max upto p.length()
+            String f = p.substring(0, i);
+            String s = p.substring(i);  // i -> end
+            count2 += permutationCount(f + ch + s, up.substring(1));
+        }
+
+        return count2;
     }
 }
